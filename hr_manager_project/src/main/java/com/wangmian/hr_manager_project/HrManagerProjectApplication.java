f672488a8f239d08202bec2be19c138c9505fde4
mengzhongjie <mengzhongjie@users.noreply.github.com>
2026-06-27 10:49:39 +0800

    后端框架搭建

---
+++ hr_manager_project/src/main/java/com/wangmian/hr_manager_project/HrManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wangmian.hr_manager_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagerProjectApplication.class, args);
+	}
+
+}
---
+++ hr_manager_project/src/test/java/com/wangmian/hr_manager_project/HrManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangmian.hr_manager_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
